baafeecbc052669fcb0951593b0b6f4974d09f8a
Mark A Guza <guzamaguza@gmail.com>
2020-03-07 02:21:39 +0000

    in progress

--- lib/user.rb
+++ lib/user.rb
@@ -3,13 +3,24 @@ require 'pry'
 require 'nokogiri'
 
 class User
-  attr_accessor :name, :
+  #attr_accessor
 
   def initialize
-
+    self
   end
 
-
-
-
+  def self.act_score(score)
+    data = Scraper.scrape_colleges
+    potential_schools = []
+    data.each do |x|
+      if x[:average_act].to_i <= score.to_i
+        #data.select{|k, hash| hash[:act_score] >= "28" }
+        #data.select{|key, hash| hash["client_id"] == "2180" }
+        potential_schools << x.partny:average_act]
+      end
+    end
+    puts potential_schools
+  end
 end
+
+User.act_score(32)
