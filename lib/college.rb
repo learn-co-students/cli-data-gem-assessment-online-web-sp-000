1818bac5b3016843da236f0255fe696142d9a8ab
Mark A Guza <guzamaguza@gmail.com>
2020-04-11 01:43:36 +0000

    refactor 4

--- lib/college.rb
+++ lib/college.rb
@@ -1,11 +1,10 @@
 class College
-  attr_accessor :name, :average_act, :average_sat, :acceptance_rate,
-  attr_accessor :public_or_private, :level_of_institution, :campus_setting
-  attr_accessor :tuition_cost, :number_of_students
+  attr_accessor :name, :college_id, :average_act, :average_sat, :acceptance_rate, :public_or_private, :level_of_institution, :campus_setting, :tuition_cost, :number_of_students
   @@all = []
 
-  def initialize(name)
+  def initialize(name,id)
     @name = name
+    @id = college_id
     @average_act = self.get_average_act
     @average_sat = self.get_average_sat
     @acceptance_rate = self.get_acceptance_rate
@@ -18,40 +17,39 @@ class College
   end
 
   def self.get_average_act
-    Scraper.scrape_act(@name)
+    Scraper.scrape_act(@name, @id)
   end
 
   def self.get_average_sat
-    Scraper.scrape_Sat(@name)
+    Scraper.scrape_Sat(@name, @id)
   end
 
   def self.get_acceptance_rate
-    Scraper.scrape_Acceptance_Rate(@name)
+    Scraper.scrape_Acceptance_Rate(@name,@id)
   end
 
   def self.get_publicorprivate
-    Scraper.scrape_PubOrPriv(@name)
+    Scraper.scrape_PubOrPriv(@name,@id)
   end
 
   def self.get_levelofinstitution
-    Scraper.scrape_LevelOfInstitution(@name)
+    Scraper.scrape_LevelOfInstitution(@name,@id)
   end
 
   def self.get_campussetting
-    Scraper.scrape_Campus_Setting(@name)
+    Scraper.scrape_Campus_Setting(@name,@id)
   end
 
   def self.get_tuitioncost
-    Scraper.scrape_Tuition_Cost(@name)
+    Scraper.scrape_Tuition_Cost(@name,@id)
   end
 
   def self.get_numofstudents
-    Scraper.scrape_NumOfStudents(@name)
+    Scraper.scrape_NumOfStudents(@name,@id)
   end
 
   def save
     @@all << self
   end
-
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,144 +3,56 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
-
-  #scrape individual college data
-  scraped_colleges = []
-  y = scraped_array.css('.college-list--card-inner').css("div.college-list--card-data-val").text
-  data_array = y.squeeze("\n").split("\n").select!{|val| !val.empty?}
-  data_array.delete(" ")
-  data_array.delete('')
-  data_array.delete(nil)
-
-  i=0
-  while i<data_array.length
-      scraped_college = {
-        :average_act => data_array[i],
-        :average_sat => data_array[i+1],
-        :acceptance_rate => data_array[i+2],
-        :public_or_private => data_array[i+3],
-        :level_of_institution => data_array[i+4],
-        :campus_setting => data_array[i+5],
-        :tuition_cost => data_array[i+6],
-        :number_of_students => data_array[i+7]
-      }
-      i = i + 8
-      scraped_colleges << scraped_college
-  end
-
-  fin_hash = {}
-  k=0
-  while k<50
-    fin_hash[name_arr[k]] = scraped_colleges[k]
-    k = k + 1
-  end
-
-
+  index_url = "https://www.cappex.com/colleges-by-act/colleges-for-a-33-act"
+  scraped_colleges = Nokogiri::HTML(open(index_url))
 
   def self.scrape_colleges
-    index_url = "https://www.cappex.com/colleges-by-act/colleges-for-a-33-act"
-    scraped_colleges = Nokogiri::HTML(open(index_url))
-    i = 0
-
+    id = 0
     scraped_colleges.css('.college-list--card-title-conatiner a').each do |x|
         name_proper = x.text.gsub(/\s+/,"_").gsub('-',"_").downcase.to_sym
-        College.new(name_proper,i)  #for every iteration of college name we create a new college instance object
-        i++
+        College.new(name_proper,id)
+        id++  #for every iteration of college name we create a new college instance object
     end
+  end
 
+  #scrape individual college data
     y = scraped_colleges.css('.college-list--card-inner').css("div.college-list--card-data-val").text
-
-  end
-
-  def self.scrape_Act(name)
-
-    y = scraped_array.css('.college-list--card-inner').css("div.college-list--card-data-val").text
-    data_array = y.squeeze("\n").split("\n").select!{|val| !val.empty?}
-    data_array.delete(" ")
-    data_array.delete('')
-    data_array.delete(nil)
-  end
-
-  def self.scrape_Sat(name)
-  end
-
-  def self.scrape_Acceptance_Rate(name)
-  end
-
-  def self.scrape_PubOrPriv(name)
-  end
-
-  def self.scrape_LevelOfInstitution(name)
-  end
-
-  def self.scrape_Campus_Setting(name)
-  end
-
-  def self.scrape_Tuition_Cost(name)
-  end
-
-  def self.scrape_NumOfStudents(name)
-  end
-
-end
-
-Scraper.scrape_colleges
-
-
-=begin
-class Scraper
-
-  def self.scrape_colleges
-
-    index_url = "https://www.cappex.com/colleges-by-act/colleges-for-a-33-act"
-
-    inner_array = []
-    scraped_array = Nokogiri::HTML(open(index_url))
-
-    #scrape college names
-    name_arr = []
-    scraped_array.css('.college-list--card-title-conatiner a').each do |x|
-        name_proper = x.text.gsub(/\s+/,"_").gsub('-',"_").downcase.to_sym
-        name_arr << name_proper
-    end
-
-    #scrape individual college data
-    scraped_colleges = []
-    y = scraped_array.css('.college-list--card-inner').css("div.college-list--card-data-val").text
     data_array = y.squeeze("\n").split("\n").select!{|val| !val.empty?}
     data_array.delete(" ")
     data_array.delete('')
     data_array.delete(nil)
 
-    i=0
-    while i<data_array.length
-        scraped_college = {
-          :average_act => data_array[i],
-          :average_sat => data_array[i+1],
-          :acceptance_rate => data_array[i+2],
-          :public_or_private => data_array[i+3],
-          :level_of_institution => data_array[i+4],
-          :campus_setting => data_array[i+5],
-          :tuition_cost => data_array[i+6],
-          :number_of_students => data_array[i+7]
-        }
-        i = i + 8
-        scraped_colleges << scraped_college
-    end
+  #make the methods below private
 
-    fin_hash = {}
-    k=0
-    while k<50
-      fin_hash[name_arr[k]] = scraped_colleges[k]
-      k = k + 1
-    end
-
-    ret_array = [name_arr, scraped_colleges, fin_hash]
-    return ret_array
+  def scrape_Act(name,id)
+    data_array[id]
   end
 
+  def scrape_Sat(name,id)
+    data_array[id+1]
+  end
+
+  def scrape_Acceptance_Rate(name,id)
+    data_array[id+2]
+  end
+
+  def scrape_PubOrPriv(name,id)
+    data_array[id+3]
+  end
+
+  def scrape_LevelOfInstitution(name,id)
+    data_array[id+4]
+  end
+
+  def scrape_Campus_Setting(name,id)
+    data_array[id+5]
+  end
+
+  def scrape_Tuition_Cost(name,id)
+    data_array[id+6]
+  end
+
+  def scrape_NumOfStudents(name,id)
+    data_array[id+7]
+  end
 end
-
-Scraper.scrape_colleges
-
-=end
