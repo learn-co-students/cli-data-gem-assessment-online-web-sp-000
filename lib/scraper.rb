53b218547b4a5072a52d5b76e8898bd566fc3b78
Mark A Guza <guzamaguza@gmail.com>
2020-04-12 19:52:45 +0000

    refactor

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,67 +5,77 @@ require 'nokogiri'
 class Scraper
   scraped_colleges = []
   index_url = "https://www.cappex.com/colleges-by-act/colleges-for-a-33-act"
-  scraped_colleges = Nokogiri::HTML(open(index_url))
+  @scraped_colleges = Nokogiri::HTML(open(index_url))
 
   def self.scrape_colleges
     id = 0
-    index_url = "https://www.cappex.com/colleges-by-act/colleges-for-a-33-act"
-    scraped_colleges = Nokogiri::HTML(open(index_url))
-    scraped_colleges.css('.college-list--card-title-conatiner a').each do |x|
+    @scraped_colleges.css('.college-list--card-title-conatiner a').each do |x|
         name_proper = x.text.gsub(/\s+/,"_").gsub('-',"_").downcase.to_sym
         College.new(name_proper,id)
-        id = id + 1   #for every iteration of college name we create a new college instance object
+        id = id + 8   #for every iteration of college name we create a new college instance object
     end
   end
 
   #scrape individual college data
 
-  y = scraped_colleges.css('.college-list--card-inner').css("div.college-list--card-data-val").text
+=begin
+  y = @scraped_colleges.css('.college-list--card-inner').css("div.college-list--card-data-val").text
   @data_array = y.squeeze("\n").split("\n").select!{|val| !val.empty?}
   @data_array.delete(" ")
   @data_array.delete('')
   @data_array.delete(nil)
-
+=end
 
   #make the methods below private
-  #private
+  private
+
   def self.scrape_college_info
-    y = scraped_colleges.css('.college-list--card-inner').css("div.college-list--card-data-val").text
+    y = @scraped_colleges.css('.college-list--card-inner').css("div.college-list--card-data-val").text
     data_array = y.squeeze("\n").split("\n").select!{|val| !val.empty?}
     data_array.delete(" ")
     data_array.delete('')
     data_array.delete(nil)
+    return data_array
   end
 
   def self.scrape_Act(name,id)
-    @data_array[id]
+    info = Scraper.scrape_college_info
+    info[id]
+    #@data_array[id]
   end
 
   def self.scrape_Sat(name,id)
-    @data_array[id+1]
+    info = Scraper.scrape_college_info
+    info[id+1]
   end
 
   def self.scrape_Acceptance_Rate(name,id)
-    @data_array[id+2]
+    info = Scraper.scrape_college_info
+    info[id+2]
   end
 
   def self.scrape_PubOrPriv(name,id)
-    @data_array[id+3]
+    info = Scraper.scrape_college_info
+    info[id+3]
   end
 
   def self.scrape_LevelOfInstitution(name,id)
-    @data_array[id+4]
+    info = Scraper.scrape_college_info
+    info[id+4]
   end
 
   def self.scrape_Campus_Setting(name,id)
-    @data_array[id+5]
+    info = Scraper.scrape_college_info
+    info[id+5]
   end
 
   def self.scrape_Tuition_Cost(name,id)
-    @data_array[id+6]
+    info = Scraper.scrape_college_info
+    info[id+6]
   end
 
   def self.scrape_NumOfStudents(name,id)
-    @data_array[id+7]
+    info = Scraper.scrape_college_info
+    info[id+7]
   end
 end
