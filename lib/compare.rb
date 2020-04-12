186e0d667f859771f73a590f1be0b26fd98768e4
Mark A Guza <guzamaguza@gmail.com>
2020-04-12 20:52:45 +0000

    update

--- lib/compare.rb
+++ lib/compare.rb
@@ -1,31 +1,42 @@
 require 'pry'
 
 class Compare
-  @@final_list = []
+  @final_list = []
 
   def self.compare_act
-    College.each do |x|
-      if User.score.to_i >=
-        @@final_list <<
-      else
+    College.each do |school|
+      if User.score.to_i >= school.average_act
+        @final_list << school.name
+      end
     end
-
   end
 
   def self.compare_price
-    User.max_payment
+    College.each do |school|
+      if User.max_payment.to_i >= school.tuition_cost
+        @final_list << school.name
+      end
+    end
   end
 
   def self.compare_preference
-    User.priv_or_pub
+    College.each do |school|
+      if User.priv_or_pub == school.public_or_private
+        @final_list << school.name
+      end
+    end
   end
 
   def self.compare_studentpop
-    User.student_pop
+    College.each do |school|
+      if User.student_pop <= school.public_or_private
+        @final_list << school.number_of_students
+      end
+    end
   end
 
   def self.display_matches
-
+    puts @final_list
   end
 
 end
