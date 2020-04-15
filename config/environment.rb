53c5ee70edcf537ec768ee18cffb6055a49ba11e
Mark A Guza <guzamaguza@gmail.com>
2020-04-15 05:50:14 +0000

    update

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,17 @@
 #require_relative '..bin/run'
 require_relative "../lib/cli"
 require_relative "../lib/college"
+require_relative "../lib/compare"
 require_relative "../lib/scraper"
 require_relative "../lib/user"
-require_relative "../lib/compare"
 
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-
+=begin
 module COLLEGES
   class Error < StandardError; end
   # Your code goes here...
 end
+=end
