22d369bc8657e7ce3a45546dc9d8cb2eabf9091f
Mark A Guza <guzamaguza@gmail.com>
2020-04-22 04:43:03 +0000

    update

---
+++ lib/college_matcher.rb
@@ -0,0 +1,15 @@
+require_relative "../lib/college_matcher/cli"
+require_relative "../lib/college_matcher/college"
+require_relative "../lib/college_matcher/compare"
+require_relative "../lib/college_matcher/scraper"
+require_relative "../lib/college_matcher/user"
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
+module CollegeMatcher
+  class Error < StandardError; end
+  # Your code goes here...
+end
